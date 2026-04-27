cbaea33ed6f09452805624a8d45c8f9a63a791c6
Rahul Dhakar <rahuldhakarmm@gmail.com>
2026-04-27 16:50:17 +0530

    fix(client): add proxy configuration for local development

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   plugins: [react(),
   tailwindcss()
   ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000'
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
